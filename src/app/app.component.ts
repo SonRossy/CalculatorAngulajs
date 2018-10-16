cb7bec9e5caea74cacd5fc423daa83f67e4c55a2
SonRossy <tristesse0889@hotmail.com>
2018-10-16 14:07:43 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CalculatorAngulajs';
+
+  getButtonsValue(buttons) {
+    const AC = document.querySelector('#calculatorInput');
+    AC.insertAdjacentHTML('beforeend', buttons);
+  }
+
+  evaluate() {
+    const value = document.querySelector('#calculatorInput');
+    try {
+      const result = (eval(value.innerHTML));
+      document.querySelector('#calculatorInput').innerHTML = result;
+    } catch (err) {
+      document.querySelector('#calculatorInput').innerHTML = err;
+    }
+
+
+  }
+  clearInput() {
+    const input = document.querySelector('#calculatorInput');
+    input.innerHTML = '';
+  }
 }
